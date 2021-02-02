fee3d4c036285732c26b762ac2e797be358d9c97
chrissoares26 <sou20001@byui.edu>
2021-02-01 21:50:32 -0700

    fixing

--- lesson5/js/currentdate.js
+++ lesson5/js/currentdate.js
@@ -1,1 +1,17 @@
 document.getElementById('currentdate').innerHTML = document.lastModified
+
+const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var d = new Date();
+var day = d.getDay(); 
+var date = d.getDate();
+var month = d.getMonth();
+var year = d.getFullYear();
+
+if (day1[day] == 'Monday') {
+    document.getElementById("announce").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    document.getElementById("announce").style.display = "block";
+}
+else {
+    document.getElementById("announce").style.display = "none";
+}
