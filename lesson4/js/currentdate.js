c86def2add816dae95fa7f72d7057074890748bc
chrissoares26 <sou20001@byui.edu>
2021-02-02 19:33:45 -0700

    date fixed

--- lesson4/js/currentdate.js
+++ lesson4/js/currentdate.js
@@ -1,1 +1,10 @@
-document.getElementById('currentdate').innerHTML = document.lastModified
+const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var d = new Date();
+var day = d.getDay(); 
+var date = d.getDate();
+var month = d.getMonth();
+var year = d.getFullYear();
+var dateStr = day1[day] + ', ' + date + ' ' + month1[month] + ' ' + year;
+document.getElementById("dates").innerHTML = dateStr;
+document.getElementById("copyright").innerHTML = year;
