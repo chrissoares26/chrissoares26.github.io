5738bed420329a3ca50a6849a007a983d6d85bc9
chrissoares26 <sou20001@byui.edu>
2021-03-02 11:37:26 -0700

    js alteration

--- lesson9/js/currentdate.js
+++ lesson9/js/currentdate.js
@@ -1,3 +1,7 @@
+function toggleMenu() {
+    document.getElementsByClassName("nav_bar")[0].classList.toggle("responsive");
+   }
+ 
 const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var d = new Date();
@@ -8,18 +12,6 @@ var year = d.getFullYear();
 var dateStr = day1[day] + ', ' + date + ' ' + month1[month] + ' ' + year;
 document.getElementById("dates").innerHTML = dateStr;
 document.getElementById("copyright").innerHTML = year;
-
-if (day1[day] == 'Friday') {
-    document.getElementById("announce").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-    document.getElementById("announce").style.display = "block";
-}
-else {
-    document.getElementById("announce").style.display = "none";
-}
-
-function adjustSeverity(rating) {
-    document.getElementById("ratingvalue").innerHTML = rating;
-}
 
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 fetch(requestURL)
@@ -60,3 +52,4 @@ fetch(requestURL)
       }
    }
 });
+
