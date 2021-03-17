d0b6307da43903191de4405fe6573f41598ac926
chrissoares26 <sou20001@byui.edu>
2021-03-16 22:52:41 -0600

    events

--- lesson11/js/preston-weather.js
+++ lesson11/js/preston-weather.js
@@ -36,3 +36,23 @@ fetch(apiURL_forecast)
   day++;	  
   });
 });
+
+//Get town events section information
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+ .then(function (response) {
+   return response.json();
+ })
+ .then(function (jsonObject) {
+   const towns = jsonObject['towns'];
+   for (let i = 0; i < towns.length; i++ ) {
+       if (towns[i].name == 'Preston') {
+        let events = towns[i].events;
+        for (let i=0; i < events.length; i++) {
+        let event = document.createElement('p');
+        event.innerHTML = events[i];
+        document.querySelector('div.townEvents').appendChild(event);
+      }
+    }
+   }});
