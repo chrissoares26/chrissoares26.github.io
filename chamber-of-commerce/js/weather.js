a5fa27c3d6f918282763d6104345c9f10b56dd48
chrissoares26 <sou20001@byui.edu>
2021-03-31 14:27:14 -0600

    Mobile version of home and join done

---
+++ chamber-of-commerce/js/weather.js
@@ -0,0 +1,41 @@
+// Current Weather for Weather Summary
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5781087&units=imperial&APPID=b4adeaa160de827fe9803c23760192cb";
+
+fetch(apiURL)
+ .then((response) => response.json())
+ .then((town) => {
+   console.log(town);
+   let description = town.weather[0].description;
+   document.getElementById('currently').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
+   document.getElementById('temp').innerHTML = Math.round(town.main.temp);
+   document.getElementById('humidity').innerHTML = town.main.humidity;
+   document.getElementById('windspeed').innerHTML = Math.round(town.wind.speed);
+});
+
+// 5 day forecast
+
+const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=5781087&units=imperial&APPID=b4adeaa160de827fe9803c23760192cb"
+
+fetch(apiURL_forecast)
+   .then(response => response.json())
+   .then((jsObject) => {
+       console.log(jsObject);
+       const forecastData = jsObject.list.filter((element)=>element.dt_txt.includes('18:00:00'));
+
+  console.log(forecastData);
+
+  const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
+   let day = 0;
+  forecastData.forEach(forecast => {
+    let x = new Date(forecast.dt_txt);
+   document.getElementById('temp'+(day+1)).textContent = Math.round(forecast.main.temp) + ' °F';
+   document.getElementById('img'+(day+1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+   document.getElementById('img'+(day+1)).alt = forecast.weather[0].description
+  document.getElementById('day'+(day+1)).textContent = weekdays[x.getDay()];
+  day++;	  
+  });
+});
+
+//Get town events section information
+
